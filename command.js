d95e266a58fbc2ce03a0d6bf47222bb7bbdf956b
AxeL-711 <zziiaadd67@gmail.com>
2026-01-28 15:36:19 +0300

    Create command.js

---
+++ command.js
@@ -0,0 +1,13 @@
+// commands.js
+const commands = [
+  {
+    name: "serverinfo",
+    description: "يعطيك معلومات عن السيرفر"
+  },
+  {
+    name: "hello",
+    description: "يسلم عليك البوت"
+  }
+];
+
+module.exports = commands;
